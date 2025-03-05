43ff46516180c90238267c4dea4f16b19994a94a
Harshavardhan Brahmandam <dev@harshavardhan.org>
2025-03-05 23:05:57 +0530

    added new mock data

--- projects/code-table/src/app/production-tables/production-tables.component.ts
+++ projects/code-table/src/app/production-tables/production-tables.component.ts
@@ -1,6 +1,6 @@
 import {
-  ChangeDetectionStrategy,
   Component,
+  DestroyRef,
   effect,
   HostBinding,
   inject,
@@ -21,10 +21,10 @@ import {
 import { NgForOf } from '@angular/common';
 import { ActivatedRoute } from '@angular/router';
 import { ProductionTableData } from './shared/types';
-import { take } from 'rxjs';
 import { SnakeCaseToStringPipe } from '../shared/pipes/snake-case-to-string.pipe';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatDivider } from '@angular/material/divider';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-production-tables',
@@ -46,7 +46,6 @@ import { MatDivider } from '@angular/material/divider';
   ],
   templateUrl: './production-tables.component.html',
   styleUrl: './production-tables.component.scss',
-  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ProductionTablesComponent {
   @HostBinding('class') className = 'h-full';
@@ -56,25 +55,28 @@ export class ProductionTablesComponent {
   protected data: ProductionTableData[] = [];
   readonly #productionTablesStore = inject(ProductionTablesStore);
   readonly #route = inject(ActivatedRoute);
+  readonly #destroyRef = inject(DestroyRef);
 
   constructor() {
     effect(() => {
-      this.#route.params.pipe(take(1)).subscribe(params => {
-        const id = params['id'];
-        const tableDetails =
-          this.#productionTablesStore.getTableDetailsById(+id);
-        if (!tableDetails) {
-          return;
-        }
-        const keys = new Set<string>();
-        tableDetails.data.forEach(table =>
-          Object.keys(table).forEach(key => keys.add(key))
-        );
-        this.displayedColumns = Array.from(keys);
-        this.columnsToDisplay = Array.from(keys);
-        this.tableName = `${tableDetails.name} Production Table`;
-        this.data = tableDetails.data;
-      });
+      this.#route.params
+        .pipe(takeUntilDestroyed(this.#destroyRef))
+        .subscribe(params => {
+          const id = params['id'];
+          const tableDetails =
+            this.#productionTablesStore.getTableDetailsById(+id);
+          if (!tableDetails) {
+            return;
+          }
+          const keys = new Set<string>();
+          tableDetails.data.forEach(table =>
+            Object.keys(table).forEach(key => keys.add(key))
+          );
+          this.displayedColumns = Array.from(keys);
+          this.columnsToDisplay = Array.from(keys);
+          this.tableName = `${tableDetails.name} Production Table`;
+          this.data = tableDetails.data;
+        });
     });
   }
 }
